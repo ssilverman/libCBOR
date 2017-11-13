7864ff854aed80f7df425632e1c1bfabd8777717
Shawn Silverman <shawn@pobox.com>
2017-11-13 11:42:36 -0800

    Adding parsing helper functions.

---
+++ src/CBOR_parsing.cpp
@@ -0,0 +1,215 @@
+#include "CBOR_parsing.h"
+
+// C++ includes
+#include <cmath>
+
+// Other includes
+#include <Arduino.h>
+
+namespace qindesign {
+namespace cbor {
+
+// ***************************************************************************
+//  Expectation functions.
+// ***************************************************************************
+
+bool expectValue(Reader r, DataType dt, uint64_t val) {
+  if (r.readDataType() != dt) {
+    return false;
+  }
+  switch (dt) {
+    case DataType::kUnsignedInt:
+      return r.getUnsignedInt() == val;
+    case DataType::kNegativeInt:
+      return r.getInt() == static_cast<int64_t>(val);
+    case DataType::kSimpleValue:
+      return r.getSimpleValue() == val;
+    case DataType::kTag:
+      return r.getTag() == val;
+    case DataType::kBytes:
+    case DataType::kText:
+    case DataType::kArray:
+    case DataType::kMap:
+      return r.getLength() == val;
+    default:
+      return false;
+  }
+}
+
+bool expectFloatValue(Reader r, float f) {
+  if (r.readDataType() != DataType::kFloat) {
+    return false;
+  }
+  float v = r.getFloat();
+  if (std::isnan(f)) {
+    return std::isnan(v);
+  }
+  if (f == 0.0f) {
+    return (v == 0) && (std::signbit(v) == std::signbit(f));
+  }
+  return v == f;
+}
+
+bool expectDoubleValue(Reader r, double d) {
+  if (r.readDataType() != DataType::kDouble) {
+    return false;
+  }
+  float v = r.getDouble();
+  if (std::isnan(d)) {
+    return std::isnan(v);
+  }
+  if (d == 0.0) {
+    return (v == 0) && (std::signbit(v) == std::signbit(d));
+  }
+  return v == d;
+}
+
+bool expectTrue(Reader r) {
+  return (r.readDataType() == DataType::kBoolean) && r.getBoolean();
+}
+
+bool expectFalse(Reader r) {
+  return (r.readDataType() == DataType::kBoolean) && !r.getBoolean();
+}
+
+bool expectUnsignedInt(Reader r, uint64_t *u) {
+  if (r.readDataType() != DataType::kUnsignedInt) {
+    return false;
+  }
+  *u = r.getUnsignedInt();
+  return true;
+}
+
+bool expectInt(Reader r, int64_t *i) {
+  if (r.readDataType() != DataType::kNegativeInt) {
+    return false;
+  }
+  *i = r.getInt();
+  return true;
+}
+
+bool expectBytes(Reader r, uint64_t *length) {
+  if (r.readDataType() != DataType::kBytes || r.isIndefiniteLength()) {
+    return false;
+  }
+  *length = r.getLength();
+  return true;
+}
+
+bool expectIndefiniteBytes(Reader r) {
+  return (r.readDataType() == DataType::kBytes) && r.isIndefiniteLength();
+}
+
+bool expectText(Reader r, uint64_t *length) {
+  if (r.readDataType() != DataType::kText || r.isIndefiniteLength()) {
+    return false;
+  }
+  *length = r.getLength();
+  return true;
+}
+
+bool expectIndefiniteText(Reader r) {
+  return (r.readDataType() == DataType::kText) && r.isIndefiniteLength();
+}
+
+bool expectArray(Reader r, uint64_t *length) {
+  if (r.readDataType() != DataType::kArray || r.isIndefiniteLength()) {
+    return false;
+  }
+  *length = r.getLength();
+  return true;
+}
+
+bool expectIndefiniteArray(Reader r) {
+  return (r.readDataType() == DataType::kArray) && r.isIndefiniteLength();
+}
+
+bool expectMap(Reader r, uint64_t *length) {
+  if (r.readDataType() != DataType::kMap || r.isIndefiniteLength()) {
+    return false;
+  }
+  *length = r.getLength();
+  return true;
+}
+
+bool expectIndefiniteMap(Reader r) {
+  return (r.readDataType() == DataType::kMap) && r.isIndefiniteLength();
+}
+
+bool expectBoolean(Reader r, bool *b) {
+  if (r.readDataType() != DataType::kBoolean) {
+    return false;
+  }
+  *b = r.getBoolean();
+  return true;
+}
+
+bool expectFloat(Reader r, float *f) {
+  if (r.readDataType() != DataType::kFloat) {
+    return false;
+  }
+  *f = r.getFloat();
+  return true;
+}
+
+bool expectDouble(Reader r, double *d) {
+  if (r.readDataType() != DataType::kDouble) {
+    return false;
+  }
+  *d = r.getDouble();
+  return true;
+}
+
+bool expectNull(Reader r) {
+  return (r.readDataType() == DataType::kNull);
+}
+
+bool expectUndefined(Reader r) {
+  return (r.readDataType() == DataType::kUndefined);
+}
+
+bool expectSimpleValue(Reader r, uint8_t *v) {
+  if (r.readDataType() != DataType::kSimpleValue) {
+    return false;
+  }
+  *v = r.getSimpleValue();
+  return true;
+}
+
+bool expectTag(Reader r, uint64_t *v) {
+  if (r.readDataType() != DataType::kTag) {
+    return false;
+  }
+  *v = r.getTag();
+  return true;
+}
+
+// ***************************************************************************
+//  Read functions.
+// ***************************************************************************
+
+unsigned int readFully(Reader r, uint8_t *b, size_t len) {
+  size_t count = 0;
+  while (true) {
+    int read = r.readBytes(b, len);
+    if (read < 0) {
+      return count;
+    }
+    count += static_cast<size_t>(read);
+    len -= static_cast<size_t>(read);
+  }
+}
+
+DataType readUntilData(Reader r) {
+  while (true) {
+    DataType dt = r.readDataType();
+    if (dt == DataType::kEOS) {
+      yield();
+      continue;
+    }
+    return dt;
+  }
+}
+
+}  // namespace cbor
+}  // namespace qindesign
---
+++ src/CBOR_parsing.h
@@ -0,0 +1,128 @@
+// CBOR_parsing.h contains functions that help with parsing CBOR data.
+
+#ifndef CBOR_PARSING_H_
+#define CBOR_PARSING_H_
+
+// C++ includes
+#include <cstdint>
+
+// Project includes
+#include "CBOR.h"
+
+namespace qindesign {
+namespace cbor {
+
+// ***************************************************************************
+//  Expectation functions. All of these functions treat end-of-stream as
+//  an unexpected value.
+// ***************************************************************************
+
+// Expects a certain type and value. This will always return false for
+// some data types having no value or a non-integral value: boolean, null,
+// undefined, break, floating-point.
+bool expectValue(Reader r, DataType dt, uint64_t val);
+
+// Expects a half- or single-precision floating-point value. Zero and
+// negative zero are considered different values.
+bool expectFloatValue(Reader r, float f);
+
+// Expects a double-precision floating-point value. Zero and negative zero
+// are considered different values.
+bool expectDoubleValue(Reader r, double d);
+
+// Expects that the next data item is a boolean and has the value 'true'.
+bool expectTrue(Reader r);
+
+// Expects that the next data item is a boolean and has the value 'false'.
+bool expectFalse(Reader r);
+
+// Expects and fills in an unsigned value. This returns true if the data item
+// is an unsigned value and false otherwise.
+bool expectUnsignedInt(Reader r, uint64_t *u);
+
+// Expects and fills in a negative value. This returns true if the data item
+// is a negative value and false otherwise.
+bool expectInt(Reader r, int64_t *i);
+
+// Expects bytes and fills in the length. This returns true if the data
+// item is non-indefinite length bytes and false otherwise.
+bool expectBytes(Reader r, uint64_t *length);
+
+// Expects bytes having indefinite length. This returns true if the data
+// item is one of these and false otherwise.
+bool expectIndefiniteBytes(Reader r);
+
+// Expects text and fills in the length. This returns true if the data
+// item is non-indefinite length text and false otherwise.
+bool expectText(Reader r, uint64_t *length);
+
+// Expects text having indefinite length. This returns true if the data
+// item is one of these and false otherwise.
+bool expectIndefiniteText(Reader r);
+
+// Expects an array and fills in the length. This returns true if the data
+// item is a non-indefinite length array and false otherwise.
+bool expectArray(Reader r, uint64_t *length);
+
+// Expects an indefinite length array. This returns true is the data item
+// is one of these and false otherwise.
+bool expectIndefiniteArray(Reader r);
+
+// Expects a map and fills in the length. This returns true if the data
+// item is a non-indefinite length map and false otherwise.
+bool expectMap(Reader r, uint64_t *length);
+
+// Expects an indefinite length map. This returns true is the data item
+// is one of these and false otherwise.
+bool expectIndefiniteMap(Reader r);
+
+// Expects and fills in a boolean value. This returns true if the data item
+// is a boolean and false otherwise.
+bool expectBoolean(Reader r, bool *b);
+
+// Expects and fills in a single-precision floating-point value. This returns
+// true if the data item is one of these and false otherwise.
+bool expectFloat(Reader r, float *f);
+
+// Expects and fills in a double-precision floating-point value. This returns
+// true if the data item is one of these and false otherwise.
+bool expectDouble(Reader r, double *d);
+
+// Expects a null value. This returns true if the data item is a null value
+// and false otherwise.
+bool expectNull(Reader r);
+
+// Expects an undefined value. This returns true if the data item is an
+// undefined value and false otherwise.
+bool expectUndefined(Reader r);
+
+// Expects and fills in a simple 1-byte value. This returns true if the data
+// item is one of these and false otherwise.
+bool expectSimpleValue(Reader r, uint8_t *v);
+
+// // Expects a non-numerical and non-boolean simple value. The set of allowed
+// // values includes null, undefined, and break.
+// // This returns true if the data item is the expected type and false otherwise.
+// bool expectSimpleValue(Reader r, DataType dt);
+
+// Expects and fills in a tag. This returns true if the data item is one
+// of these and false otherwise.
+bool expectTag(Reader r, uint64_t *v);
+
+// ***************************************************************************
+//  Read functions.
+// ***************************************************************************
+
+// Attempts to read exactly len bytes into b. This will return the actual
+// number of bytes read, a smaller number than len only if the stream
+// encountered end-of-stream.
+unsigned int readFully(Reader r, uint8_t *b, size_t len);
+
+// Reads from the reader until there's some data available. If end-of-stream
+// is encountered then this yields and keeps looking. Forever.
+DataType readUntilData(Reader r);
+
+}  // namespace cbor
+}  // namespace qindesign
+
+#endif  // CBOR_PARSING_H_
